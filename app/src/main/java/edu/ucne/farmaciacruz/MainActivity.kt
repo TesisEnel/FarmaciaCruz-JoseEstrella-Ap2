4b4bfc63a0fcd157d69b0e38777ca2102e8d3a5b
Jose Gabriel <josegabrielestrella04@gmail.com>
2025-11-21 21:35:53 -0400

    Eliminado MainViewModel

--- app/src/main/java/edu/ucne/farmaciacruz/MainActivity.kt
+++ app/src/main/java/edu/ucne/farmaciacruz/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.farmaciacruz.presentation.navigation.AppNavigation
 import edu.ucne.farmaciacruz.ui.theme.FarmaciaCruzTheme
@@ -17,10 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val mainViewModel: MainViewModel = hiltViewModel()
-            val isDarkTheme by mainViewModel.isDarkTheme.collectAsState()
-
-            FarmaciaCruzTheme(darkTheme = isDarkTheme) {
+            FarmaciaCruzTheme {
                 AppNavigation()
             }
         }
