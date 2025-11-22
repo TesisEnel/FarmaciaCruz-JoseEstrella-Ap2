d07bb2f27475652887f319094e0e3ebcd12db06b
Jose Gabriel <josegabrielestrella04@gmail.com>
2025-11-21 22:25:37 -0400

    Added DatabaseModule

---
+++ app/src/main/java/edu/ucne/farmaciacruz/di/DatabaseModule.kt
@@ -0,0 +1,45 @@
+package edu.ucne.farmaciacruz.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.farmaciacruz.data.local.FarmaciaDatabase
+import edu.ucne.farmaciacruz.data.local.dao.CarritoDao
+import edu.ucne.farmaciacruz.data.repository.CarritoRepositoryImpl
+import edu.ucne.farmaciacruz.domain.repository.CarritoRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFarmaciaDatabase(
+        @ApplicationContext context: Context
+    ): FarmaciaDatabase {
+        return Room.databaseBuilder(
+            context,
+            FarmaciaDatabase::class.java,
+            "farmacia_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCarritoDao(database: FarmaciaDatabase): CarritoDao {
+        return database.carritoDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCarritoRepository(
+        carritoDao: CarritoDao
+    ): CarritoRepository {
+        return CarritoRepositoryImpl(carritoDao)
+    }
+}
