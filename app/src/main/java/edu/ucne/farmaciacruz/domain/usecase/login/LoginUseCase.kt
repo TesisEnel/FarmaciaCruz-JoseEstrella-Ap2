84432588af7c3032943cf99728a994a060166454
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-11-22 18:48:32 -0400

    change UseCase to domian interface

--- app/src/main/java/edu/ucne/farmaciacruz/domain/usecase/login/LoginUseCase.kt
+++ app/src/main/java/edu/ucne/farmaciacruz/domain/usecase/login/LoginUseCase.kt
@@ -1,15 +1,16 @@
 package edu.ucne.farmaciacruz.domain.usecase.login
 
 import android.util.Patterns
-import edu.ucne.farmaciacruz.data.repository.AuthRepositoryImpl
+
 import edu.ucne.farmaciacruz.domain.model.Resource
 import edu.ucne.farmaciacruz.domain.model.User
+import edu.ucne.farmaciacruz.domain.repository.AuthRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(
-    private val authRepository: AuthRepositoryImpl
+    private val authRepository: AuthRepository
 ) {
     suspend operator fun invoke(email: String, password: String): Flow<Resource<User>> {
         if (email.isBlank()) {
--- app/src/main/java/edu/ucne/farmaciacruz/domain/usecase/producto/GetProductosUseCase.kt
+++ app/src/main/java/edu/ucne/farmaciacruz/domain/usecase/producto/GetProductosUseCase.kt
@@ -1,15 +1,15 @@
 package edu.ucne.farmaciacruz.domain.usecase.producto
 
-import edu.ucne.farmaciacruz.data.repository.ProductRepositoryImpl
 import edu.ucne.farmaciacruz.domain.model.Producto
 import edu.ucne.farmaciacruz.domain.model.Resource
+import edu.ucne.farmaciacruz.domain.repository.ProductRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetProductosUseCase @Inject constructor(
-    private val productRepositoryImpl: ProductRepositoryImpl
+    private val productRepository: ProductRepository
 ) {
     suspend operator fun invoke(): Flow<Resource<List<Producto>>> {
-        return productRepositoryImpl.getProductos()
+        return productRepository.getProductos()
     }
 }
